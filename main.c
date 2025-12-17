9ac129404da91a868897c6ed79bd1c41f694b490
Dmytro <foxi.deus@protonmail.com>
2025-12-17 13:22:15 +0000

    init

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void logo(void) {
+    printf("  / _ \\ \n");
+    printf("\\_\\(_)/_/ \n");
+    printf(" _//o\\_  \n");
+    printf("  /   \\ \n");
+}
+
+int main() {
+    logo();
+    return 0;
+}   
