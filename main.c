25a75a75872fd84eb04cf8df3ad9706654870100
Dmytro <foxi.deus@protonmail.com>
2025-12-17 13:46:15 +0000

    added logo

--- main.c
+++ main.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
+#include <time.h>
 
-void logo(void) {
-    printf("  / _ \\ \n");
-    printf("\\_\\(_)/_/ \n");
-    printf(" _//o\\_  \n");
-    printf("  /   \\ \n");
+void logo(const char *name, const char *github) {
+    static char buf[9];
+    time_t now = time(NULL);
+    struct tm t;
+    localtime_r(&now, &t);
+
+    printf("  / _ \\   %02d:%02d:%02d\n",
+           t.tm_hour, t.tm_min, t.tm_sec);
+    printf("\\_\\(_)/_/ %s\n", name);
+    printf(" _//o\\_   %s\n", github);
+    printf("  /   \\ \n\n");
 }
 
 int main() {
-    logo();
+    logo("Dmytro Shvydenko", "github.com/CrudelisDeus");
     return 0;
-}   
+}
